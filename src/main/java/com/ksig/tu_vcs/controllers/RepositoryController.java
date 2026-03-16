8dc93156bbcc4f20f77ab8c1310d765167a834a3
Radina01 <radina_mihaylova0101@abv.bg>
2026-03-16 14:55:59 +0200

    add repository view

--- src/main/java/com/ksig/tu_vcs/controllers/RepositoryController.java
+++ src/main/java/com/ksig/tu_vcs/controllers/RepositoryController.java
@@ -1,8 +1,8 @@
 package com.ksig.tu_vcs.controllers;
 
-import com.ksig.tu_vcs.repos.dto.RepositoryRequest;
 import com.ksig.tu_vcs.repos.entities.Repository;
 import com.ksig.tu_vcs.services.RepositoryService;
+import com.ksig.tu_vcs.services.views.RepositoryView;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,11 +16,13 @@ public class RepositoryController {
     }
 
     @PostMapping
-    public Repository createRepository(@RequestBody RepositoryRequest request) {
+    public Repository createRepository(@RequestBody RepositoryView view) {
         return repositoryService.createRepository(
-                request.name(),
-                request.description(),
-                request.ownerId()
+                view.getRepositoryName(),
+                view.getDescription(),
+                view.getOwnerId()
         );
     }
+
+
 }
---
+++ src/main/java/com/ksig/tu_vcs/services/views/RepositoryView.java
@@ -0,0 +1,23 @@
+package com.ksig.tu_vcs.services.views;
+
+import com.ksig.tu_vcs.repos.entities.Repository;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class RepositoryView {
+    private String repositoryName;
+    private String description;
+    private UUID ownerId;
+
+    public static RepositoryView fromEntity(Repository entity) {
+        RepositoryView view = new RepositoryView();
+        view.repositoryName = entity.getName();
+        view.description = entity.getDescription();
+        view.ownerId = entity.getOwnerID();
+        return view;
+    }
+}
