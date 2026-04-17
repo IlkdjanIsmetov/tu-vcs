bae37131a5e935f2485a23f11e0a6b81a9ffa2b2
IlkdjanIsmetov <ilkdjanismetov@gmail.com>
2026-04-17 13:19:43 +0300

    changed enpoinds for pull controller

--- src/main/java/com/ksig/tu_vcs/controllers/PullController.java
+++ src/main/java/com/ksig/tu_vcs/controllers/PullController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/pull")
+@RequestMapping("/api/repositories")
 public class PullController {
     private final PullService pullService;
 
